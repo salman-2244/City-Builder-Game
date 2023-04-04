c6676b088aa59265ae27b3aa179c05d8a438a60a
o311pi <o311pi@inf.elte.hu>
2023-04-04 09:30:49 +0200

    Adding to field

--- game/Field.py
+++ game/Field.py
@@ -1,11 +1,11 @@
 from Zones.zone import Zone
 class Field:
-    def __init__(self, x, y, size, zone):
+    def __init__(self, x, y, size):
         self.x = x
         self.y = y
-        self.size = size
+        self.size = size # size of the individual tile
         self.zone = None # zone , when selcted change to the zone it is in
         self.road = False # bool to check if there is a road on the field
         
-
-
+    def set_zone(self, zone):
+        self.zone = zone
