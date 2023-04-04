fa82973d11d3ffb4b6a654570f19183301b9b17e
o311pi <o311pi@inf.elte.hu>
2023-04-04 09:23:22 +0200

    zone changes

--- game/Field.py
+++ game/Field.py
@@ -1,9 +1,11 @@
+from Zones.zone import Zone
 class Field:
     def __init__(self, x, y, size, zone):
         self.x = x
         self.y = y
         self.size = size
-        self.zone = zone
+        self.zone = None # zone , when selcted change to the zone it is in
+        self.road = False # bool to check if there is a road on the field
         
 
 
